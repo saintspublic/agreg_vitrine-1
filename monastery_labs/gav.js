acace9d785832955475fee467d983f73e7d15bbb
GestorSaints <saints.gestor@gmail.com>
2026-06-10 22:55:14 -0300

    Revert ".monastery"
    This reverts commit c8117da6b3c24796a942bcd99b79d22afde10637.

---
+++ monastery_labs/gav.js
@@ -0,0 +1,7 @@
+document
+    .getElementById("sala-estudo")
+    .addEventListener("click", () => {
+
+        window.location.href = "https://gestorsaints.github.io/agreg_vitrine/ascet_space/recepcao.html";
+
+    });
